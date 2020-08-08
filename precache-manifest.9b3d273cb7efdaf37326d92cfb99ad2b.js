2dae5691804f62a8aa0245f37106f212f81bc488
Rzepal <max.rzepkowski@gmail.com>
2020-08-08 23:41:18 +0200

    Updates

---
+++ precache-manifest.9b3d273cb7efdaf37326d92cfb99ad2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd41465abaf6a7e184d1c8afa2307d4",
+    "url": "/infcinema/index.html"
+  },
+  {
+    "revision": "233c8d71f356ac57a0b8",
+    "url": "/infcinema/static/css/main.dc3e7da5.chunk.css"
+  },
+  {
+    "revision": "9e645d2bc125d11238a9",
+    "url": "/infcinema/static/js/2.492267ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/infcinema/static/js/2.492267ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233c8d71f356ac57a0b8",
+    "url": "/infcinema/static/js/main.ca03f82b.chunk.js"
+  },
+  {
+    "revision": "0b4fcd593b4130bdcaa3",
+    "url": "/infcinema/static/js/runtime-main.ac0915ea.js"
+  },
+  {
+    "revision": "1747a0fb943f37d941a46f8e0bd32d61",
+    "url": "/infcinema/static/media/empty_actor.1747a0fb.jpg"
+  },
+  {
+    "revision": "06f945e1e5ef5f5c8023e11ec80f9edf",
+    "url": "/infcinema/static/media/empty_poster.06f945e1.jpg"
+  }
+]);
